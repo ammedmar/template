c11bd2db3cb9505c0033b80c40fbb142b49020df
ammedmar <ammedmar@gmail.com>
2021-08-26 10:26:27 +0200

    Add creation of displaycmds.tex file in the .py file

--- aux/displaycmds.py
+++ aux/displaycmds.py
@@ -1,27 +1,28 @@
 '''
-prints all commands defined in usualcmd.tex
-copy and paste into displycmds.tex
+Reads defined commands in and creates tex file to render them all
+
 '''
 
 import re
+import os
 
 skip = {'pdfEinfty', 'anibal'}
 
-with open('usualcmds.tex') as f:
+with open('usualcmds.tex', 'r') as f:
     contents = f.read()
 
     pattern1 = re.compile(r'[dr]{(.+?)}{')
     matches1 = pattern1.finditer(contents)
-    for m in matches1:
-        if all([w not in m[1] for w in skip]):
-            print(f'\\verb|{m[1]}|, ${m[1]}$ ;')
-
-    print('\n')
 
     pattern2 = re.compile(r'[dr]{(.+?)}\[(\d)\]')
     matches2 = pattern2.finditer(contents)
-    for m in matches2:
-        if m[2] == '1':
-            print(f'\\verb|{m[1]}{{x}}"|, ${m[1]}{{x}}$ ;')
-        if m[2] == '2':
-            print(f'\\verb|{m[1]}{{x}}{{y}}"|, ${m[1]}{{x}}{{y}}$ ;')
+
+    with open('displaycmds.tex', 'w') as fp:
+        for m in matches1:
+            if all([w not in m[1] for w in skip]):
+                fp.write(f'\\verb|{m[1]}|, ${m[1]}$ ; \n')
+        for m in matches2:
+            if m[2] == '1':
+                fp.write(f'\\verb|{m[1]}{{x}}|, ${m[1]}{{x}}$ ; \n')
+            if m[2] == '2':
+                fp.write(f'\\verb|{m[1]}{{x}}{{y}}|, ${m[1]}{{x}}{{y}}$ ; \n')
